cb9c735949b132436e6f01df24f6f1b292e2b4aa
dexterhere04 <sdrtharun@gmail.com>
2026-01-06 07:15:23 +0530

    final preview

--- app/OceanBackground.tsx
+++ app/OceanBackground.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 function WaterfallPlane() {
   const materialRef = useRef<THREE.ShaderMaterial | null>(null);
