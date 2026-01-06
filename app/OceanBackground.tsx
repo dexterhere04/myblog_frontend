aa766042569ac6b7f5b11b9c47af7052ed8f9869
dexterhere04 <sdrtharun@gmail.com>
2026-01-06 07:12:56 +0530

    remove unused imports

--- app/OceanBackground.tsx
+++ app/OceanBackground.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import * as THREE from "three";
 
 function WaterfallPlane() {
   const materialRef = useRef<THREE.ShaderMaterial | null>(null);
